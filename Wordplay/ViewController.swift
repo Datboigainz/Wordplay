a444701c1dacbda49e670c2e85d4f1677420b6bb
Ruben rojo <61779@psdschools.org>
2021-10-28 10:13:06 -0600

    mvp

--- Wordplay/ViewController.swift
+++ Wordplay/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let nextViewcontroller = segue.destination as!
+        ViewController1
+        
+        var tree = textField.text
+        nextViewcontroller.datboi37 = tree ?? ""
+    }
+    
+    
 }
 
---
+++ Wordplay/ViewController1.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController1.swift
+//  Wordplay
+//
+//  Created by Ruben Rojo on 10/28/21.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    @IBOutlet weak var newButton: UILabel!
+    var datboi37 = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newButton.text = datboi37
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
